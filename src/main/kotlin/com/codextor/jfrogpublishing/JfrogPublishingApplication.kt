307e9a817bc6ff4411b44f2f3a5caea54d6a7a9b
Sandeep Pandey <fromsandeeppandey@gmail.com>
2022-07-15 17:02:17 +0530

    Initial commit

---
+++ src/main/kotlin/com/codextor/jfrogpublishing/JfrogPublishingApplication.kt
@@ -0,0 +1,11 @@
+package com.codextor.jfrogpublishing
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JfrogPublishingApplication
+
+fun main(args: Array<String>) {
+	runApplication<JfrogPublishingApplication>(*args)
+}
---
+++ src/test/kotlin/com/codextor/jfrogpublishing/JfrogPublishingApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.codextor.jfrogpublishing
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JfrogPublishingApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
