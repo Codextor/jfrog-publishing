68e75471dad4acbf63ff4b79d8c8d3605632abb7
Sandeep Pandey <fromsandeeppandey@gmail.com>
2022-07-15 17:07:52 +0530

    Add sample rest controller

---
+++ src/main/kotlin/com/codextor/jfrogpublishing/Greeting.kt
@@ -0,0 +1,3 @@
+package com.codextor.jfrogpublishing
+
+class Greeting(val id: Long, val content: String)
--- src/main/kotlin/com/codextor/jfrogpublishing/JfrogPublishingApplication.kt
+++ src/main/kotlin/com/codextor/jfrogpublishing/JfrogPublishingApplication.kt
@@ -7,5 +7,5 @@ import org.springframework.boot.runApplication
 class JfrogPublishingApplication
 
 fun main(args: Array<String>) {
-	runApplication<JfrogPublishingApplication>(*args)
+    runApplication<JfrogPublishingApplication>(*args)
 }
